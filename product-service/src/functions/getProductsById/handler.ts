fcafd2a566e680953c8885cc6731081b1ba20ddd
Anna Kiniova <anna.kiniova@gmail.com>
2021-09-07 01:25:32 +0300

    task_4. Add pg client and requests

--- product-service/src/functions/getProductsById/handler.ts
+++ product-service/src/functions/getProductsById/handler.ts
@@ -1,8 +1,21 @@
 import "source-map-support/register";
 import { formatJSONResponse } from "@libs/apiGateway";
 import { middyfy } from "@libs/lambda";
-import products from "../products.json";
+import { Client } from "pg";
 import { ErrorCode, ErrorMessage } from "../../errors";
+
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+const dbOptions = {
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeoutMillis: 5000,
+};
 
 const getProductsById = async (event) => {
   let productId: string;
@@ -15,15 +28,32 @@ const getProductsById = async (event) => {
     );
   }
 
-  const selectedProduct = products.find((item) => item.id === productId);
+  const client = new Client(dbOptions);
+  await client.connect();
 
-  const response = selectedProduct
-    ? formatJSONResponse(selectedProduct)
-    : formatJSONResponse(
-        { message: ErrorMessage.NOT_FOUND },
-        ErrorCode.NOT_FOUND
-      );
-  return response;
+  try {
+    console.log(productId);
+    const { rows: selectedProduct } = await client.query(
+      `SELECT * 
+      FROM products AS T1 
+        LEFT JOIN stock AS T2 ON
+          T1.id = T2.product_id
+      WHERE T1.id = $1`,
+      [productId]
+    );
+    console.log(selectedProduct);
+    const response = selectedProduct
+      ? formatJSONResponse(selectedProduct[0])
+      : formatJSONResponse(
+          { message: ErrorMessage.NOT_FOUND },
+          ErrorCode.NOT_FOUND
+        );
+    return response;
+  } catch (err) {
+    formatJSONResponse(err.message, ErrorCode.SERVER_ERROR);
+  } finally {
+    client.end();
+  }
 };
 
 export const main = middyfy(getProductsById);
